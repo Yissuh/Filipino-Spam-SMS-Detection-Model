fddd3443da865d9d949521d622d96bbeaef4d3e5
Yissuh <alexrosete18@gmail.com>
2023-12-08 16:53:18 +0800

    Update

--- brands.py
+++ brands.py
@@ -1,4 +1,4 @@
-all_brands = ("Youtube", "Facebook", "Twitter", "TM", "Lazada", "GCash", "Smart", "Bioessence", "Shopee", "Google", "BDO", "IOS", "PAYMAYA", "Maya"
+all_brands = ("Youtube", "Facebook", "Twitter", "TM", "Lazada", "GCash", "Smart", "Bioessence", "Shopee", "Google", "BDO", "IOS", "PAYMAYA", "MAYA"
         "BPI", "LandBank", "LANDBANK", "HONDA", "BINANCE", "BONDEE", "PayPal", "Pag-IBIG", "J&T",
         "Twitter", "Telegram", "Xoom", "Netflix", "Angkas", "Viu", "Joyride", "Jollibee",
         "Angel's Pizza", "Xiaomi", "Globe", "7-Eleven", "CASHBEE", "GOMO", "Converge", "Gsave", "Gigalife", "Salvatore", "SM", "Angkas", "Joyride", "Moveit", "NDRRMC", "PhiVolcs")
